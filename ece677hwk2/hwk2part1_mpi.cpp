8654937b3a0356c60f0ec3d925c78b6d83acb67e
scott <scottswindell@email.arizona.edu>
2018-09-16 20:12:08 -0700

    part1 mpi seems to be working thought not more efficiently than serialy

--- ece677hwk2/hwk2part1_mpi.cpp
+++ ece677hwk2/hwk2part1_mpi.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <random>
-
+#include <mpi.h>
+#include <math.h>
+#include <assert.h>
 #define MATSIZEX 128
 #define MATSIZEY 128
 #define RANGE_HIGH 31
@@ -41,37 +43,123 @@ void inline count( unsigned char matrix[MATSIZEX][MATSIZEY], size_t count_vals[]
 	}
 }
 
-
-
-void print_matrix(unsigned char matrix[MATSIZEX][MATSIZEY])
+void inline count_onenum( unsigned char matrix[MATSIZEX][MATSIZEY], size_t count_vals[], int num )
 {
-	for (int ii=0; ii<MATSIZEX; ++ii)
-	{
-		for(int jj=0; jj< MATSIZEY; ++jj)
+		count_vals[num]=0;
+		for (int ii=0; ii<MATSIZEX; ++ii)
 		{
-			std::cout << (int)matrix[ii][jj] << " ";
+			for(int jj=0; jj< MATSIZEY; ++jj)
+			{
+				if(matrix[ii][jj] == num)
+					count_vals[num]++;
+			}
 		}
-		std::cout << std::endl;
-	}
 
+}
+
+void inline count_chunk( unsigned char matrix[MATSIZEX][MATSIZEY], int count_vals[], size_t startx, size_t starty, size_t chunksizex, size_t chunksizey)
+{
+	assert((startx+chunksizex) <= MATSIZEX );
+	assert((starty+chunksizey) <= MATSIZEY );
+	for(int kk=RANGE_LOW; kk<RANGE_HIGH+1; kk++)
+	{
+		count_vals[kk]=0;
+		for (int ii=startx; ii<startx+chunksizex; ++ii)
+		{
+			for(int jj=starty; jj<starty+chunksizey; ++jj)
+			{
+				if(matrix[ii][jj] == kk)
+				{
+					count_vals[kk]++;
+				}
+			}
+		}
+
+	}
+}
+
+void print_matrix(unsigned char matrix[][MATSIZEY], int startx, int starty, int width, int height)
+{
+	for (int ii=startx; ii<startx+width; ++ii)
+	{
+		for(int jj=starty; jj< starty+height; ++jj)
+		{
+			printf("% 3d, ", matrix[ii][jj]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	
+
+}
+
+void print_count(int count_vals[])
+{
+	int sum;
+	for(size_t index=0; index<RANGE_HIGH+1; index++)
+	{
+		std::cout << index<< ": " << count_vals[index] << " ";
+		sum=sum+count_vals[index];
+	}
+	std::cout << std::endl << "SUM IS " << sum << " But should be "<< MATSIZEX*MATSIZEY<<std::endl;
 }
 
 int main()
 {
-	
 	unsigned char matrix[MATSIZEX][MATSIZEY];
-	size_t count_vals[RANGE_HIGH];
-	populate_matrix(matrix);
-	count(matrix, count_vals);
-	int sum=0;
-	for(size_t index; index<RANGE_HIGH+1; index++)
+	int rank, size, ii, jj;
+	int local_count_vals[RANGE_HIGH+1];
+	int global_count_vals[RANGE_HIGH+1];
+	int fudge_factor=0;
+	int startx, starty, width, height, sides;
+
+	for(int ii=0; ii<=RANGE_HIGH; ii++)
 	{
-		//std::cout << index<< ": " << count_vals[index] << " " << std::endl;
-		sum=sum+count_vals[index];
+		local_count_vals[ii]=0;
+		global_count_vals[ii]=0;
 	}
-	//std::cout << "SUM IS " << sum << " But should be "<< 128*128<<std::endl;
+
+	populate_matrix(matrix);
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	
+
+
+	count_chunk(matrix, local_count_vals, (rank)*MATSIZEX/size, 0, MATSIZEX/size, MATSIZEY );
+	/*printf("rank: %d, chunkx: %d-%d, chunky: %d-%d\n", 
+			rank, 
+			(rank)*MATSIZEX/size, 
+			(rank)*MATSIZEX/size+MATSIZEX/size,  
+			(rank)*MATSIZEY/size, 
+			(rank)*MATSIZEY/size+MATSIZEX/size);
+	*/
+	if (rank == 0)
+	{
+		print_matrix(matrix, (rank)*MATSIZEX/(size), 0, MATSIZEX/(size), MATSIZEY );
+		print_count(local_count_vals);
+	}
+	MPI_Reduce(local_count_vals, global_count_vals, RANGE_HIGH+1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	int sum=0;
+
+	if(rank == 0)
+	{
+		//print_matrix(matrix, 0,0,MATSIZEX, MATSIZEY);
+		/*
+		for(size_t index=0; index<RANGE_HIGH+1; index++)
+		{
+			std::cout << index<< ": " << global_count_vals[index] << " ";
+			sum=sum+global_count_vals[index];
+		}
+		std::cout << std::endl << "SUM IS " << sum << " But should be "<< MATSIZEX*MATSIZEY<<std::endl;
+		*/
+		print_count(global_count_vals);
+	}
+
+	
+	MPI_Finalize();
 }
 
 
